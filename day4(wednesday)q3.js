4885fba9915ac70576dc066cb6683c5ca811de41
anuraga27 <68176221+anuraga27@users.noreply.github.com>
2020-07-16 10:14:12 +0530

    Add files via upload

---
+++ day4(wednesday)q3.js
@@ -0,0 +1,18 @@
+ console.log("Hello")
+
+// These all are modals. Modals are those popups, which dont allow you to interact with other part of the webpage unless you have interacted with the modal popup.
+
+// Alert
+// alert("hey I am an alert");
+
+// Prompt
+// syntax: prompt("title", "default value"[optional])
+ let age = prompt("What is your age");
+ console.log(age);
+ let age = confirm("Are you over 21?");
+ console.log(age);
+ if(age>=21)
+ {
+console.log("can drink");
+ } 
+console.log("cannot drink");
